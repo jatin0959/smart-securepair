c76b969f39eb7adbcabf5d3edff408d58bc9f2dd
jatin0959 <dubeyjatin0959@gmail.com>
2025-07-14 08:00:20 +0530

    cd

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import LayoutClient from '@/components/LayoutClient'; // the client-side loader wrapper
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] });
 const geistMono = Geist_Mono({ variable: '--font-geist-mono', subsets: ['latin'] });
 
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="antialiased">
+        <Analytics/>
         <LayoutClient>{children}</LayoutClient>
       </body>
     </html>
